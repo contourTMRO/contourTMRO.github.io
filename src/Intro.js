88e80ee5388d270bb21e42626e7eef1bb91558cd
rajmulherkar1 <rajmul@umich.edu>
2023-11-28 14:01:12 -0500

    Updating Intro Slide

--- src/Intro.js
+++ src/Intro.js
@@ -4,7 +4,8 @@ import React from 'react';
 function Intro() {
     return (
         <div class="body">
-            Intro Content
+            <h1>Welcome to the Radiation Oncology Training Module!</h1>
+            <h2>Please select the option that fits you best below!</h2>
         </div>
     );
 }
